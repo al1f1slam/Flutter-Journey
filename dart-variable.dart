847963bff45a1039e95beee51484b0b3c3ecd434
Alif Islam Siam <65067674+al1f1slam@users.noreply.github.com>
2024-01-08 16:05:47 +0600

    Add files via upload

---
+++ dart-variable.dart
@@ -0,0 +1,20 @@
+void main(){
+
+  var x=10;
+  var y=40;
+  var z=x+y;
+
+  var a=5.5;
+  var b=5.5;
+  var c=a+b;
+
+  print(x);
+  print(y);
+  print(z);
+
+  print(a);
+  print(b);
+  print(c);
+
+
+}
