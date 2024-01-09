df324dd463dae234e4663d9c9938a469304ce0ee
Alif Islam Siam <65067674+al1f1slam@users.noreply.github.com>
2024-01-09 12:45:51 +0600

    Add files via upload

---
+++ dart-map.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  var person={
+    'name':'pranti',
+    'age':20,
+    'address':'Banasree',
+    'education':'LLB',
+    'status':'Mingle'
+  };
+
+  print(person);
+  print(person['name']);
+  print(person['age']);
+  print(person['address']);
+  print(person['education']);
+  print(person['status']);
+
+}
