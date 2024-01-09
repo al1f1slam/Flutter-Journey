084c482b6f8163e26c0d126815d12e11b31264f4
Alif Islam Siam <65067674+al1f1slam@users.noreply.github.com>
2024-01-09 12:45:32 +0600

    Add files via upload

---
+++ dart-operators.dart
@@ -0,0 +1,35 @@
+void main(){
+
+  var a=10;
+  var b=5;
+
+  print("Arithmatic operators:");
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+  print(a%b);
+
+  print("Unary operators:");
+  var x=20;
+  print(x++);
+  print(x++);
+
+  var y=25;
+  print(++y);
+  print(++y);
+
+  var z=30;
+  print(z--);
+  print(z--);
+
+  var s=35;
+  print(--s);
+  print(--s);
+
+
+
+
+
+
+}
