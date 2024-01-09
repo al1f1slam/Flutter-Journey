ad3cfa63511444d97353c8037a48efa6f50959f1
Alif Islam Siam <65067674+al1f1slam@users.noreply.github.com>
2024-01-10 00:50:55 +0600

    Add files via upload

---
+++ dart-list_properties.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  var city=['Dhaka','Cumilla','Noakhali', 100, 100.50, true, false ];
+
+  var result0= city.length;
+  var result1= city.first;
+  var result2= city.isEmpty;
+  var result3= city.isNotEmpty;
+  var result4= city.last;
+  var result5= city.reversed;
+
+
+  print(result0);
+  print(result1);
+  print(result2);
+  print(result3);
+  print(result4);
+  print(result5);
+
+
+
+}
