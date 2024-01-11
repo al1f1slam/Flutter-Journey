820c880593e6e6bc686080f27341fd4bd5e9e3f0
Alif Islam Siam <65067674+al1f1slam@users.noreply.github.com>
2024-01-12 01:42:03 +0600

    Add files via upload

---
+++ dart-fixed_length_list.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  var city=['Dhaka','Cumilla','Noakhali'];
+  print(city);
+
+  city.add("Rajshahi");
+  city.add("Khulna");
+  print(city);
+
+  const city2=['Dhaka','Jatrabari'];
+  print(city2);
+
+
+
+
+}
